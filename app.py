d5b53bc91f603297be50cbc23961dc3f163ce5e8
chris rudzki <chrisrudzki@chriss-MacBook-Air.local>
2026-03-07 17:40:43 -0800

    changed app.py

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+
+# current_string = {"value": ""}
+
+# @app.route('/update', methods=['POST'])
+# def update():
+#     data = request.json
+#     current_string["value"] = data["value"]
+#     return jsonify({"status": "ok"})
+
+# @app.route('/get', methods=['GET'])
+# def get():
+#     return jsonify(current_string)
+
+# if __name__ == '__main__':
+#     app.run()
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -13,6 +32,10 @@ def update():
 @app.route('/get', methods=['GET'])
 def get():
     return jsonify(current_string)
+
+@app.route('/')
+def home():
+    return "Server is running"
 
 if __name__ == '__main__':
     app.run()
